e3e1f1e7f62007e21e962cd95e4a419b0a1bcfe0
enzapps <snippebucket@gmail.com>
2022-06-28 17:44:23 +0530

    <yt>

---
+++ README.rst
@@ -0,0 +1,6 @@
+Default tree view for contacts
+===============================
+
+It change default contacts view to tree instead of kanban.
+
+This module is developed by the `KitWorks <https://kitworks.systems/>`__.
---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Default tree view for contacts',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Productivity',
+    'license': 'OPL-1',
+    'version': '14.0.1.0.1',
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'views/partner_view.xml',
+    ],
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+}
